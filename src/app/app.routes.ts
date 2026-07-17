db7f516e131bf56c9bc96d10082f79ab2981673a
Frederic Mas <fredfredfredm@yahoo.fr>
2026-07-17 11:29:39 +0200

    refacto/home-componen:  create page-title component with styles and add it to home page, add theme style in styles.scss, update app.route for have page display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,26 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent as HomeComponentBis } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CountryComponent } from "./pages/country/country.component";
+import { HomeComponent } from './feature/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponentBis,
+  },
+  {
+    path: 'home',
     component: HomeComponent,
   },
   {
-    path : 'country/:countryName',
-    component : CountryComponent
+    path: 'country/:countryName',
+    component: CountryComponent
   },
 
   {
-    path : 'not-found',
-    component : NotFoundComponent
+    path: 'not-found',
+    component: NotFoundComponent
   },
   {
     path: '**',
--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Olympic } from 'src/app/core/models/olympic.model';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+import { PageTitleComponent } from "src/app/shared/components/page-title/page-title.component";
 
 @Component({
-  selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+    selector: 'app-home',
+    imports: [PageTitleComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+    // attributes
+    protected pageTitle: string = "Medals per Country";
+    private olympics!: Observable<Olympic[] | undefined>;
+
+    // contructor
+    private olympicService = inject(OlympicService);
+
+    ngOnInit() {
+        this.olympics = this.olympicService.fetchOlympics();
+        this.olympics.pipe(
+            map(value => console.log(value))
+        ).subscribe();
+    }
+
+
+    buildPieChart(countries: string[], sumOfAllMedalsYears: number[]) {
+
+    }
+}
