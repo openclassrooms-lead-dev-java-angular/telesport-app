608c8f59879678baf07324ff58d81cf765947e0f
Frederic Mas <fredfredfredm@yahoo.fr>
2026-07-17 15:39:31 +0200

    refacto/home-componen:  create pie-chart component and add it to home component

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -5,13 +5,15 @@ import { Olympic } from 'src/app/core/models/olympic.model';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { PageTitleComponent } from "src/app/shared/components/page-title/page-title.component";
 import { StatisticCardComponent } from 'src/app/shared/components/statistic-card/statistic-card.component';
-import Chart from 'chart.js/auto';
+import { ChartData } from 'src/app/core/models/chart-data.model';
+import { PieChartComponent } from 'src/app/shared/components/pie-chart/pie-chart.component';
 
 @Component({
     selector: 'app-home',
     imports: [
         PageTitleComponent,
         StatisticCardComponent,
+        PieChartComponent,
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
@@ -21,7 +23,15 @@ export class HomeComponent implements OnInit {
     protected pageTitle: string = "Medals per Country";
     protected totalJOs = signal(0);
     protected totalCountries = signal(0);
-    protected pieChart!: Chart<"pie", number[], string>;
+    protected chartData = signal<ChartData>({
+        labels: [],
+        datasets: [{
+            label: '',
+            data: [],
+            backgroundColor: [],
+            hoverOffset: 0
+        }]
+    });
     private olympics!: Observable<Olympic[] | undefined>;
 
     // contructor
@@ -38,7 +48,7 @@ export class HomeComponent implements OnInit {
         });
     }
 
-    updateStatitics(data: Olympic[]) {
+    updateStatitics(data: Olympic[]): void {
         this.totalCountries.set(data.length);
         this.totalJOs.set(
             Array.from(
@@ -48,6 +58,22 @@ export class HomeComponent implements OnInit {
     }
 
     buildPieChart(data: Olympic[]) {
+        const countries: string[] = data.map((i: any) => i.country);
+        const medals = data.map((i: any) => i.participations.map((i: any) => (i.medalsCount)));
+        const sumOfAllMedalsYears = medals.map((i) => i.reduce((acc: any, i: any) => acc + i, 0));
 
+        this.chartData.set({
+            labels: countries,
+            datasets: [{
+                label: 'Medals',
+                data: sumOfAllMedalsYears,
+                backgroundColor: ['#0b868f', '#adc3de', '#7a3c53', '#8f6263', 'orange', '#94819d'],
+                hoverOffset: 4
+            }]
+        })
+    }
+
+    navigateToCountry(label: string): void {
+        this.router.navigate(['country', label]);
     }
 }
