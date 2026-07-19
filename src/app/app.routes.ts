e14c46c64a7a108f99cc1d54c9b9563c7c55da39
Frederic Mas <fredfredfredm@yahoo.fr>
2026-07-19 14:04:49 +0200

    refacto/not-found-component:  create not-found component and add it to route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NotFoundComponent } from './feature/not-found/not-found.component';
 import { CountryComponent } from "./pages/country/country.component";
 
 export const routes: Routes = [
