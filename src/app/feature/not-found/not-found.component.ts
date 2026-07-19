44072eed30fe3eff9d27a093ecc3f00222691c1a
Frederic Mas <fredfredfredm@yahoo.fr>
2026-07-19 22:25:23 +0200

    feat/button-component:  add to not found component

--- src/app/feature/not-found/not-found.component.ts
+++ src/app/feature/not-found/not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "src/app/shared/components/button/button.component";
 
 @Component({
     selector: 'app-not-found',
-    imports: [],
+    imports: [ButtonComponent],
     templateUrl: './not-found.component.html',
     styleUrl: './not-found.component.scss',
     standalone: true
