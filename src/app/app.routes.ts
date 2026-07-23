b358c850b4da516ec63654907670fed3393131ae
Frederic Mas <fredfredfredm@yahoo.fr>
2026-07-23 09:52:34 +0200

    fix(routes): udpate app routes with renamed components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './feature/not-found/not-found.component';
-import { CountryComponent } from './feature/country/country.component';
-import { HomeComponent } from './feature/home/home.component';
+import { CountryDetailComponent } from './feature/country-detail/country-detail.component';
+import { MedalDashboardComponent } from './feature/medal-dashboard/medal-dashboard.component';
+
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
+        component: MedalDashboardComponent,
     },
     {
         path: 'country/:countryId',
-        component: CountryComponent,
+        component: CountryDetailComponent,
     },
     {
         path: '**',
